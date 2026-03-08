acc5fae56c649657994d6b447e9a4881e1984a5d
Balamurugan896 <balamurugank896@gmail.com>
2026-03-08 14:45:25 +0530

    Update data_logger.js

--- data_logger.js
+++ data_logger.js
@@ -44,6 +44,8 @@ app.post("/raw_logs", async (req, res) => {
   res.json({ status: "log stored" })
 })
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
